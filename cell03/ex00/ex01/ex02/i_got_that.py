f14d74b2d9ce2a2382f0678f3f44b01d72d5352c
Kancharos272 <67030272@kmitl.ac.th>
2024-11-21 06:07:10 +0000

    stop

---
+++ cell03/ex00/ex01/ex02/i_got_that.py
@@ -0,0 +1,12 @@
+
+
+while True:
+    
+    user_input = input("What you gotta say? : ")
+    
+   
+    print(f"I got that! Anything else? : {user_input}")
+    
+  
+    if user_input.lower() == "stop":
+        break
